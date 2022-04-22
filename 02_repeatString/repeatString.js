d070ef9535c95c2faedd33e8f5de982dbfc1effb
Tom Rintoul <thomas.rintoul@icloud.com>
2022-04-22 17:18:03 +0100

    finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(x, y) {
+    let phrase = "";
+    
+    if (y >= 1) {
+        while (y != 0) {
+            phrase += x;
+            y--;
+        }
+        return phrase;
+    } 
+    else if (y < 0)
+    {
+        return 'ERROR';
+    }
+    else {
+        return "";
+    }
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
