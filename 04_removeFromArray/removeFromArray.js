aefd83d8e8f1c1bfc350280888e46bf686c8225c
Tom Rintoul <thomas.rintoul@icloud.com>
2022-04-23 08:46:30 +0100

    finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    const newArray = [];
+    const argValues = [];
 
+    theArgs.forEach((argument) => {
+        argValues.push(argument)
+    })
+
+    for (let x = 0; x < array.length; x++) {
+        if (argValues.includes(array[x]) == false) {
+            newArray.push(array[x]);
+        }
+    }
+    return newArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
