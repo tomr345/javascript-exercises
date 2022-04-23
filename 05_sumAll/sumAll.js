2c3114f58fe75f7cf25309cc7263ac4ce9389708
Tom Rintoul <thomas.rintoul@icloud.com>
2022-04-23 11:25:19 +0100

    finished exercises off

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x > 0 && y > 0 && Number.isInteger(x) && Number.isInteger(y)) {
+        let first = 0;
+        let last = 0;
+
+        if (y > x) {
+            first = x;
+            last = y;
+        }
+        else {
+            first = y;
+            last = x;
+        }
+        
+        let sum = 0;
+        let counter = first;
+
+        while (counter <= last) {
+            sum += counter;
+            counter++;
+        }
+
+        return sum;
+    }
+    else
+    {
+        return 'ERROR';
+    }
 
 };
 
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
