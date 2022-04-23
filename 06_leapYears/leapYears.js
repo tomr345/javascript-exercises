c3afffdb90234e00a93aac90e7d7383f2c7bbf34
Tom Rintoul <thomas.rintoul@icloud.com>
2022-04-23 11:35:31 +0100

    finished exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
+const leapYears = function(x) {
+    let year = x;
 
+    if (x % 4 == 0) {
+        if (x % 100 == 0) {
+            if (x % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    return false;
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(1985));
+console.log(leapYears(1984));
 
 // Do not edit below this line
 module.exports = leapYears;
