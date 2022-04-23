aac89eed5f2f207fcb793c60a9603a4222bd2d3e
Tom Rintoul <thomas.rintoul@icloud.com>
2022-04-23 08:07:01 +0100

    finished exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
 
+    for (let y = (string.length - 1); y > -1; y--) {
+        newString += string[y];
+    }                    
+
+    return newString;
 };
+
+console.log(reverseString("hello world"));
 
 // Do not edit below this line
 module.exports = reverseString;
