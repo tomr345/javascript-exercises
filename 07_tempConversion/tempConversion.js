0670f2a6c8321ab385cba3c0406ed69f6c50a98c
Tom Rintoul <thomas.rintoul@icloud.com>
2022-04-23 14:49:35 +0100

    finished exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,21 @@
-const ftoc = function() {
+const ftoc = function(x) {
+  let f = x;
+  let temp = (f - 32) * (5 / 9);
+  let final = Math.round(temp * 10) / 10;
 
+  return final;
 };
 
-const ctof = function() {
+const ctof = function(x) {
+  let c = x;
+  let temp = (c * (9 / 5)) + 32;
+  let final = Math.round(temp * 10) / 10;
 
+  return final;
 };
+
+console.log(ftoc(100));
+console.log(ctof(39));
 
 // Do not edit below this line
 module.exports = {
